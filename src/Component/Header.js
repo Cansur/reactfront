059ee96039873a1177e3a39d287a13a86de2f276
Cansur <tjswo0682@gmail.com>
2024-07-02 22:36:11 +0900

    Minecraft

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
 		<>
 			<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
 				<div class="container-fluid padding-10 margin-left-10">
-					<Link to="/" className='text-decoration-line-none'><span class="navbar-brand mb-0 h1">Minecraftd</span></Link>
+					<Link to="/" className='text-decoration-line-none'><span class="navbar-brand mb-0 h1">Minecraft</span></Link>
 				</div>
 			</nav>
 		</>
